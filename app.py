b5338ffa5c7be3abec5c3e07f0d7a2472de4a25c
Atharv Mane <140147402+AtharvMane123@users.noreply.github.com>
2024-12-19 21:05:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify, request
+import pickle
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+model = pickle.load(open('pipe (5).pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello World'
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+    area = request.form.get('area')
+    bedrooms = request.form.get('bedrooms')	
+    bathrooms =request.form.get('bathrooms')
+    mainroad =  request.form.get('mainroad')	
+    guestroom =  request.form.get('guestroom') 
+    basement =  request.form.get('basement')
+    hotwaterheating =  request.form.get('hotwaterheating')	
+    airconditioning =  request.form.get('airconditioning')
+    parking =  request.form.get('parking')
+    furnishingstatus =  request.form.get('furnishingstatus')
+
+    dictionary = {'area':area,
+              'bedrooms':bedrooms,
+              'bathrooms':bathrooms,
+              'mainroad':mainroad,
+              'guestroom':guestroom,
+              'basement':basement,
+              'hotwaterheating':hotwaterheating,
+              'airconditioning':airconditioning,
+              'parking':parking,
+              'furnishingstatus':furnishingstatus
+    }
+
+    test_input = np.array([1210,2,1,'yes','yes','yes','yes','yes',2,'furnished'],dtype = object).reshape(1,10)   
+    result  = model.predict(test_input)
+    print(result)
+    return jsonify({'placement':str(result)})
+
+if __name__ == '__main__':
+    app.run(debug = True)
